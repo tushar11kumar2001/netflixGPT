8cd9a15fe02787a0200f63af2599d318b028eeb5
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-01 02:56:00 +0530

    netlify

--- src/components/browser/VideoBackground.jsx
+++ src/components/browser/VideoBackground.jsx
@@ -1,17 +1,18 @@
 const VideoBackground = ({ trailerKey }) => {
-
+  console.log("trailer key :",trailerKey)
   return (
     <div className="overflow-hidden w-screen h-screen ">
 
-      {trailerKey &&<><iframe 
+      {trailerKey &&
+      <>
+      <iframe 
       className="w-full h-[120%] relative top-[-50px]"  
       src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&mute=1&loop=1`} 
       title="YouTube video player"
-
       ></iframe>
-
       </>
       }
+      
     </div>
   )
 }
